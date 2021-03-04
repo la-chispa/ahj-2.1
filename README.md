77642ae019d8935b6306f8141aba96ffb9734487
Olga Belotserkovskaya <olla.chispa@gmail.com>
2021-03-05 02:44:01 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/suuf6ngdbi4himrk?svg=true)](https://ci.appveyor.com/project/la-chispa/ahj-2-1)
+
+Github-pages: https://la-chispa.github.io/ahj-2.1/
---
+++ src/js/__tests__/renderBoard.test.js
@@ -0,0 +1,10 @@
+import { expect, test } from '@jest/globals';
+import renderBoard from '../renderBoard';
+
+test('board should be rendered', () => {
+  document.body.innerHTML = '<div class="container"></div>';
+  renderBoard(4);
+  const cells = document.getElementsByClassName('cell');
+  expect(cells.length).toBe(16);
+  expect(document.querySelector('.board')).toBeTruthy();
+});
